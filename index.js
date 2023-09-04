610e4524270c265985913f29aef6763439505684
ADev <battlegom@outlook.kr>
2023-09-04 21:17:50 +0900

    an update

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
 var JSDocument = require('jsdocument')
 
-var document = new JSDocument.JSDocument()
-var title = new JSDocument.JSObjects.JSTitleObject() // creates new title object
+var index = new JSDocument.JSPage()
 var h1 =  new JSDocument.JSObjects.JSH1Object()
 var text = new JSDocument.JSObjects.JSPObject()
 
-JSDocument.UseCSS(document, 'index.css')
-JSDocument.UseIcon(document, 'jsdocument.ico')
+index.SetTitle("Hello, world!")
+
+JSDocument.UseCSS(index.GetJSDocumentObject(), 'index.css', JSDocument.Server)
+JSDocument.UseIcon(index.GetJSDocumentObject(), 'jsdocument.ico', JSDocument.Server)
 
 h1.Text = 'JSDocument' // innerHTML
 text.Text = 'My First JSDocument Page!' // innerHTML
-title.Text = 'Hello, World!' // innerHTML
 
-document.AddHeaderObject(title) // adds object on head tag, use on title tag or script tag.
 document.AddObject(h1)
 document.AddObject(text) // adds object on body tag
 
-document.setresponsehandler(function() {
-    document.SendDocument() // sends document to client
-}, '/')
+index.Deploy('/') // Deploys site on index
